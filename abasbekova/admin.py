0e3cb5f16e199d785538a0892c7c3e9f15d0b3c7
qboshenkiy <fittsqboshenkiy@gmail.com>
2024-06-05 15:56:53 +0600

    ff

--- abasbekova/admin.py
+++ abasbekova/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import addcard
 # Register your models here.
+@admin.register(addcard)
+class adminCard(admin.ModelAdmin):
+    list_display = ['title', 'text', 'image']
